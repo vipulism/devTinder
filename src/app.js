343c77b3a5accb21fd2ebf78b3b98d3d493a48f3
Vipulism <vipul0809@gmail.com>
2024-12-15 23:58:18 +0530

    apis

--- src/app.js
+++ src/app.js
@@ -22,15 +22,83 @@ connectDb()
 
 app.post('/signup', async (req, res) => {
 
-    const user = new User(req.body)
+  const user = new User(req.body)
 
   try {
-    await user.save();
-    res.send("user add");
+
+    const isUser = await User.findOne({emailId:user.emailId});
+    if(isUser){
+      res.status(400).send("User Already Exist");
+    }else{
+      await user.save();
+      res.send("user add");
+    }
   } catch (error) {
     res.status(400).send("err in user signup ");
   }
-
      
 });
+
+app.get('/user', async (req, res) => {
+  const {emailId} = req.body;
+  
+  try {
+    const user = await User.findOne({emailId});
+    if(user){
+      res.send(user);
+    } else {
+      res.status(404).send("user not found")
+    }
+  } catch (error) {
+    res.status(400).send("err in getting user");
+  }
+});
+
+
+app.get('/feed', async (req, res) => {
+
+  try {
+    const users = await User.find({});
+    if(users.length){
+      res.send(users);
+    } else {
+      res.status(404).send("user not found")
+    }
+  } catch (error) {
+    res.status(400).send("err in getting feed");
+  }
+});
+
+
+app.delete('/user', async (req, res) => {
+  const {userId} = req.body;
+
+  try {
+      await User.findByIdAndDelete(userId);
+      res.send('user deleted');
+  } catch (error) {
+    res.status(400).send("err in deleting user");
+  }
+});
+
+
+app.patch('/user', async (req, res) => {
+
+  const { userId, ...user} = req.body;
+
+
+
+  try {
+
+    await User.findOneAndUpdate({emailId:user.emailId}, user)
+
+    // await User.findByIdAndUpdate(userId, user);
+    res.send("user updated");
+
+  } catch (error) {
+    res.status(400).send("err in user signup ");
+  }
+     
+});
+
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type:String,
+        required:true,
     },
     lastName: {
         type:String,
     },
     emailId: {
         type:String,
+        required:true,
     },
     password: {
         type:String,
+        required:true,
     },
     age: {
         type:String,
