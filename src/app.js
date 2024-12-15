db8588418f5842c2da67ad0bb118f4720b75ca10
Vipulism <vipul0809@gmail.com>
2024-12-15 22:14:32 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -3,10 +3,37 @@ const app = express();
 const {userAuth} = require('./middleware/auth');
 const {connectDb} = require("./config/database");
 
-connectDb().then(() => {
+const User = require('./models/user')
+
+connectDb()
+.then(() => {
 
     console.log("db connected...");
     app.listen(7080, () => {
         console.log("Server is sucsssfully listening on port 7080")
     });
+}).catch((err) => {
+    console.log("Server cannot connect");
+    
 })
+
+
+app.post('/signup', async (req, res) => {
+
+    const user = new User({
+        firstName:"Vipul",
+        lastName:"Sharma",
+        emailId:"vipul0809@gmail.com",
+        password:"vipul@132"
+    })
+
+  try {
+    await user.save();
+    res.send("user add");
+  } catch (error) {
+    res.status(400).send("err in user signup ");
+  }
+
+     
+});
+
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 //  "mongodb+srv://vipul0809:zq0G77k1FmpxFsKB@cluster0.urvqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?directConnection=true"
 
 const connectDb = async () => {
-    await mongoose.connect("mongodb+srv://vipul0809:zq0G77k1FmpxFsKB@cluster0.urvqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?directConnection=true");
+    await mongoose.connect("mongodb+srv://vipul0809:zq0G77k1FmpxFsKB@cluster0.urvqc.mongodb.net/devTinder");
 };
 
 module.exports = { connectDb };
